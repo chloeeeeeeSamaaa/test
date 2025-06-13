968f004d0dc2a6a5c6cdf7ce215f79e23e6d13a1
chloeeeeE <165114652+chloeeeeeeSamaaa@users.noreply.github.com>
2025-06-13 14:40:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu =  document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.scroll = () =>   {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text',{
+        strings: ['Frontend Developer', 'Backend Developer', 'Gui Developer', 'Web Designer', 'Tiktoker'],
+        typeSpeed:80,
+        backSpeed: 80,
+        backDelay: 1200,
+        loop: true,
+}
+);
+
